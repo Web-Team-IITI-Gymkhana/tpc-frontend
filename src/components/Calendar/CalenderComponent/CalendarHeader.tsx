5092724ff35cf35d4055e02e1d3590d645b4493e
PrkharMishra <136120437+Prkhar05@users.noreply.github.com>
2024-03-27 15:49:10 +0530

    Removed Commented out codes

--- src/components/Calendar/CalenderComponent/CalendarHeader.tsx
+++ src/components/Calendar/CalenderComponent/CalendarHeader.tsx
@@ -20,9 +20,6 @@ export default function CalendarHeader() {
 
   return (
     <header className="px-4 py-2 flex items-center">
-      {/* <h1 className="mr-10 text-xl text-gray-500 fond-bold">
-        Calendar
-      </h1> */}
       <button
         onClick={handleReset}
         className="border border-gray-500 rounded py-2 px-2  mr-5 hover:bg-blue-600 hover:text-white hover:border-blue-950 "
--- src/components/Calendar/CalenderComponent/SidebarCalendar.tsx
+++ src/components/Calendar/CalenderComponent/SidebarCalendar.tsx
@@ -5,23 +5,9 @@ import GlobalContext from '../context/GlobalContext'
 
 
 export default function SidebarCalendar() {
-  // const { monthIndex, setMonthIndex } = useContext(GlobalContext);
 
-  // function handleReset() {
-  //   setMonthIndex(
-  //     monthIndex === dayjs().month()
-  //       ? monthIndex + Math.random()
-  //       : dayjs().month()
-  //   );
-  // }
   return (
     <aside className='border p-5 w-40'>
-      {/* <button
-        onClick={handleReset}
-        className="border border-gray-500 rounded py-2 px-2 mt-10 "
-      >
-        Today
-      </button> */}
         <Labels />
     </aside>
   )
