b76fb14e4832e1d91467ce7e495edea10afb6d10
PrkharMishra <136120437+Prkhar05@users.noreply.github.com>
2024-03-29 12:00:51 +0530

    added key to <React.Fragment>

--- src/components/Calendar/CalenderComponent/Month.tsx
+++ src/components/Calendar/CalenderComponent/Month.tsx
@@ -6,7 +6,7 @@ export default function Month({month}:{month:any}) {
   return (
     <div className='flex-1 grid grid-cols-7 grid-rows-5'>
         {month.map((row: any[],i: any) => (
-            <React.Fragment>
+            <React.Fragment key={i}>
                 {row.map((day,idx)=>(
                     <Day day={day} key={idx} rowIdx={i} />
                 ))}
