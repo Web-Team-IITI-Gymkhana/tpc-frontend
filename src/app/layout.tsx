d78645188737b88a39799618d6d21f08077a3137
Prince <x.prince.x@outlook.com>
2024-04-03 21:39:40 +0530

    Removed the previous navbar commented code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,15 +33,6 @@ const RootLayout = async ({ children, auth }: Props) => {
       <body className={className}>
         <ToggleProvider>
           <div className="flex flex-col">
-            {/* <nav className="shadow-xl bg-gray-900">
-              <div className="max-w-7xl mx-auto px-2 sm:px-4 flex justify-between items-center h-[8vh]">
-                <div className="flex-shrink-0 flex items-center">
-                  <Link href="/" className="text-white font-bold text-2xl">
-                    <div className="hidden sm:block mx-2">TPC</div>
-                  </Link>
-                </div>
-              </div>
-            </nav> */}
             {/* Page Content */}
             <Toaster />
 
