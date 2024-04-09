99dd3c46f2d98741a9ab04f16ae6167a196940d0
AKSHAT SHARMA <akshatgopal70@gmail.com>
2024-04-09 18:43:23 +0530

    fixed dropdow position of Change role

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -113,10 +113,10 @@ const Sidebar = () => {
             <DropdownTrigger>
               <Button
                 variant="bordered"
-                className="py-[0.5vw]  rounded-lg mb-[1vw] text-yellow-500 pl-[0.5vw]"
+                className="py-[0.5vw] rounded-lg mb-[1vw] text-yellow-500 pl-[0.5vw]"
                 // startContent={<ChevronDown />}
               >
-                <span className="fixed top-[85.5vh] left-[1vw]">
+                <span className="fixed top-[71.5vh] left-[1vw]">
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>
                     </span>
                 <motion.span
@@ -245,7 +245,14 @@ const Sidebar = () => {
           </div>
           <hr />
           <NavButtonGroup />
+
+
+          
         </div>
+
+
+
+
         {isAdmin && (
           <div>
             <Link href="/">
