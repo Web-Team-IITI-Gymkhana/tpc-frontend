2cf469e29d21f4eb731537b804bc4c175eb2d829
AryanGKulkarni <95529889+AryanGKulkarni@users.noreply.github.com>
2024-07-14 20:52:14 +0530

    removed get auth function

--- src/app/(authroutes)/login/layout.tsx
+++ src/app/(authroutes)/login/layout.tsx
@@ -1,18 +1,7 @@
 import React from "react";
-import { apiCall } from  '@/helpers/api';
-import toast from "react-hot-toast";
 
 interface Props {
   children: React.ReactNode;
-}
-
-async function getAuth() {
-  try {
-    const response = await apiCall("/auth");
-    return response;
-  } catch (error) {
-    toast.error("Error Authenticating");
-  }
 }
 
 const LoginModalLayout = ({ children }: Props) => {
