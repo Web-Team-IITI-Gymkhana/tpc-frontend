67237b1cb75e8932f2379f105132c6c7af25e365
Prince Kumar <x.prince.x@outlook.com>
2024-07-19 17:19:30 +0530

    Company table (#132)
    Co-authored-by: AryanGKulkarni <95529889+AryanGKulkarni@users.noreply.github.com>

--- src/app/(routes)/admin/companies/page.tsx
+++ src/app/(routes)/admin/companies/page.tsx
@@ -1,5 +1,49 @@
-import React from "react";
+"use client";
+import { fetchcompanies } from "@/helpers/api";
+import generateColumns from "@/components/NewTableComponent/ColumnMapping";
+import { companyDTO } from "@/dto/CompanyDto";
+import Table from "@/components/NewTableComponent/Table";
+import { useEffect, useState } from "react";
+import Loader from "@/components/Loader/loader";
 
-export default function page() {
-  return <div>page</div>;
-}
+const hiddenColumns = ["id"];
+
+const JobPage = () => {
+  const columns = generateColumns(companyDTO);
+  const [loading, setLoading] = useState(true);
+  const [allCompanies, setAllCompanies] = useState();
+  const visibleColumns = columns.filter(
+    (column: any) => !hiddenColumns.includes(column?.accessorKey),
+  );
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await fetchcompanies();
+      setAllCompanies(data);
+      setLoading(false);
+    };
+    getData();
+  }, []);
+
+  return (
+    <div className="m-10">
+      <h1 className="text-center font-bold text-3xl my-5 py-5">Companies</h1>
+      <div>
+        {loading && (
+          <div className="h-48 flex justify-center items-center">
+            <Loader />
+          </div>
+        )}
+        {allCompanies && (
+          <Table
+            data={allCompanies}
+            columns={visibleColumns}
+            type={"company"}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default JobPage;
