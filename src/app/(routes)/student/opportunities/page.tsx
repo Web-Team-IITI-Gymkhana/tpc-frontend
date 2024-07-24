94d78e72178647744031020fc6f5d24bebfe7f19
AryanGKulkarni <95529889+AryanGKulkarni@users.noreply.github.com>
2024-07-24 16:11:11 +0530

    opportunities added

--- src/app/(routes)/student/opportunities/page.tsx
+++ src/app/(routes)/student/opportunities/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import JobCard from "@/components/jobs/JobCard";
-import { GetJobs } from "@/helpers/student/api";
+import { GetOpportuinites } from "@/helpers/student/api";
 import { Jobs } from "@/helpers/student/types";
 import toast from "react-hot-toast";
 import Loader from "@/components/Loader/loader";
@@ -12,7 +12,7 @@ const StudentPage = () => {
   useEffect(() => {
     const fetchJobs = async () => {
       try {
-        const data = await GetJobs();
+        const data = await GetOpportuinites();
         setJobs(data);
       } catch (error) {
         toast.error("Error fetching data:");
@@ -27,7 +27,7 @@ const StudentPage = () => {
   return (
     <div>
       <div className="my-3 mx-5 font-bold text-xl">
-        <h1>Jobs</h1>
+        <h1>Apply</h1>
       </div>
       <div>
         {loading && (
