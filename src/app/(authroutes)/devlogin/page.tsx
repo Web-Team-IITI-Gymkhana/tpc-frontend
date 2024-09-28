8d953c2d35b6485d65c5f17dd00d33d0432e669d
AryanGKulkarni <95529889+AryanGKulkarni@users.noreply.github.com>
2024-09-28 21:17:03 +0530

    login fixed

---
+++ src/app/(authroutes)/devlogin/page.tsx
@@ -0,0 +1,9 @@
+//this will be used to create the login page.
+import React from "react";
+import LoginForm from "@/components/loginForms/devLoginForm";
+
+const LoginPage = async () => {
+  return <LoginForm />;
+};
+
+export default LoginPage;
--- src/app/(authroutes)/login/page.tsx
+++ src/app/(authroutes)/login/page.tsx
@@ -3,7 +3,13 @@ import React from "react";
 import LoginForm from "@/components/loginForms/loginForm";
 
 const LoginPage = async () => {
-  return <LoginForm />;
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="md:w-1/4 w-[45vw]">
+        <LoginForm />
+      </div>
+    </div>
+  )
 };
 
 export default LoginPage;
