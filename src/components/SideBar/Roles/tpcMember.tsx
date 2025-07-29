b3496d14ec8df4ae5aca48616b75bc225c3374b0
JaiPannu-IITI <pannujai06iiti@gmail.com>
2025-07-29 11:33:21 +0530

    Access granted

--- src/components/SideBar/Roles/tpcMember.tsx
+++ src/components/SideBar/Roles/tpcMember.tsx
@@ -551,6 +551,43 @@ const TpcMemberDashboard = () => {
                 {/* <SessionDropDown AllSeasons={AllSeasons} /> */}
               </div>
             </Link>
+            <Link href={"/admin/dashboard"}>
+              <div className="hover:bg-slate-600/50 rounded-md my-2 py-2 px-4 cursor-pointer text-white">
+                <div className="flex justify-start gap-3">
+                  <div className="w-5 flex-shrink-0 text-white">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 32 32"
+                      width="20"
+                      height="20"
+                      className="text-white"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M24 21h2v5h-2zm-4-5h2v10h-2zm-9 10a5.006 5.006 0 0 1-5-5h2a3 3 0 1 0 3-3v-2a5 5 0 0 1 0 10"
+                      ></path>
+                      <path
+                        fill="currentColor"
+                        d="M28 2H4a2 2 0 0 0-2 2v24a2 2.003 2.003 0 0 0 2 2h24a2.003 2.003 0 0 0 2-2V4a2 2 0 0 0-2-2m0 9H14V4h14ZM12 4v7H4V4ZM4 28V13h24l.002 15Z"
+                      ></path>
+                    </svg>
+                  </div>
+                  <motion.div
+                    initial={{ opacity: 1 }}
+                    animate={context.isOpen ? "open" : "closed"}
+                    transition={{ duration: 0.1 }}
+                    variants={{
+                      closed: { opacity: 0 },
+                      open: { opacity: 1 },
+                    }}
+                    className="flex-1 text-white"
+                  >
+                    Dashboard
+                  </motion.div>
+                </div>
+                {/* <SessionDropDown AllSeasons={AllSeasons} /> */}
+              </div>
+            </Link>
             <Link href={"/events"}>
               <div className="hover:bg-slate-600/50 rounded-md my-2 py-2 px-4 cursor-pointer text-white">
                 <div className="flex justify-start gap-3">
