3427e7b49d9497faad3a4e511836b8922cb7212b
Prince Kumar <x.prince.x@outlook.com>
2026-07-07 15:26:00 +0530

    Refactor makeOffer function to handle multiple student IDs for job offers

--- src/components/Admin/JobEvents.tsx
+++ src/components/Admin/JobEvents.tsx
@@ -801,13 +801,13 @@ const MakeJobOfferModal = ({
   ]);
 
   const makeOffer = async (salaryId: string) => {
-    await postOnCampusOffer([
-      {
-        salaryId: salaryId,
-        studentId: studentIds[0],
+    await postOnCampusOffer(
+      studentIds.map((studentId) => ({
+        salaryId,
+        studentId,
         status: "ACCEPTED",
-      },
-    ]);
+      })),
+    );
     toast.success("Made a successful offer!");
     window.location.reload();
   };
@@ -816,7 +816,7 @@ const MakeJobOfferModal = ({
     const fetchSalaries = async () => {
       const salaries = await getStudentSalaryOffers(
         lastEvent.job.id,
-        studentIds[0],
+          studentIds[0],
       );
       const newSalaries = salaries.map((salary) => ({
         select: (
